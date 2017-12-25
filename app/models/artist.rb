8329817d2ed89a58d15fc9eaedcbade1dbeb8f1d
christelva <cvanaanholt@hotmail.com>
2017-12-25 22:31:00 +0100

    Added validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,7 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
+  
+  validates :name, presence: true
+
+  validates :bio, length: { maximum: 600 }
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+  validates :title, presence: true
+  validates :album, presence: true
+  validates :year, presence: true
 end
