bc53ae5162fa990c0c3682dc76e0d5f0f6092457
christelva <cvanaanholt@hotmail.com>
2017-12-26 16:16:02 +0100

    updated controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,38 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :destroy]
+
+  def index
+    @songs = Song.all
+  end
+
+  def show; end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to root_path, notice: "Song created"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to root_path, notice: "Song removed"
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :artist_id)
+  end
+
 end
