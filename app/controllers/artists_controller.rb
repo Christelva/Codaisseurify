19becbc459834ba7dee6a51f8d6b541b8477c533
christelva <cvanaanholt@hotmail.com>
2018-01-01 15:58:08 +0100

    deleted stuff in artists controller Im not using

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,43 +9,9 @@ def show
   @photos = @artist.photos
 end
 
-def new
-  @artist = Artist.new
-end
-
-def create
-  @artist = Artist.new(artist_params)
-
-  if @artist.save
-    image_params.each do |image|
-      @artist.photos.create(image: image)
-    end
-
-    redirect_to @artist, notice: "Artist is successfully created"
-  else
-    render 'new'
-  end
-end
-
-def edit
-  @photos = @artist.photos
-end
-
-def update
-  if @artist.update(artist_params)
-    image_params.each do |image|
-      @artist.photos.create(image: image)
-    end
-
-    redirect_to edit_artist_path(@artist), notice: "Artist is successfully updated"
-  else
-    render :edit
-  end
-end
-
 def destroy
   @artist.destroy
-  redirect_to root_path, notice: "Artist is successfully removed"
+  redirect_to root_path, notice: "Artist removed!"
 end
 
 private
